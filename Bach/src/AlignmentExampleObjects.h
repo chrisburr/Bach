937aee4ee49eb0bc9c29bfd49740d19449400ae7
Christopher Burr <christopher.burr@cern.ch>
2017-01-09 19:14:43 +0100

    Run clang format on AlignmentExampleObjects.h

--- Bach/src/AlignmentExampleObjects.h
+++ Bach/src/AlignmentExampleObjects.h
@@ -14,121 +14,127 @@
 #define DD4HEP_ALIGNDET_ALIGNMENTEXAMPLEOBJECTS_H
 
 // Framework include files
-#include "DD4hep/LCDD.h"
-#include "DD4hep/Printout.h"
-#include "DD4hep/Conditions.h"
-#include "DD4hep/Alignments.h"
+#include "DD4hep/AlignedVolumePrinter.h"
 #include "DD4hep/AlignmentData.h"
+#include "DD4hep/Alignments.h"
+#include "DD4hep/AlignmentsProcessor.h"
+#include "DD4hep/Conditions.h"
 #include "DD4hep/DetAlign.h"
 #include "DD4hep/DetConditions.h"
 #include "DD4hep/DetectorProcessor.h"
-#include "DD4hep/AlignmentsProcessor.h"
-#include "DD4hep/AlignedVolumePrinter.h"
+#include "DD4hep/LCDD.h"
+#include "DD4hep/Printout.h"
 
-#include "DDCond/ConditionsSlice.h"
-#include "DDCond/ConditionsManager.h"
 #include "DDAlign/AlignmentsManager.h"
+#include "DDCond/ConditionsManager.h"
+#include "DDCond/ConditionsSlice.h"
 
 /// Namespace for the AIDA detector description toolkit
 namespace DD4hep {
 
-  /// Namespace for alignment examples
-  namespace AlignmentExamples {
+/// Namespace for alignment examples
+namespace AlignmentExamples {
 
-    using Geometry::LCDD;
-    using Geometry::RotationZYX;
-    using Geometry::DetElement;
-    using Geometry::DetectorProcessor;
-    
-    using Conditions::UserPool;
-    using Conditions::Condition;
-    using Conditions::ConditionKey;
-    using Conditions::ConditionsPool;
-    using Conditions::ConditionsSlice;
-    using Conditions::ConditionsManager;
-    using Conditions::DetConditions;
+using Geometry::LCDD;
+using Geometry::RotationZYX;
+using Geometry::DetElement;
+using Geometry::DetectorProcessor;
 
-    using Alignments::Delta;
-    using Alignments::DetAlign;
-    using Alignments::Alignment;
-    using Alignments::AlignmentsManager;
-    
-    /// Example how to populate the detector description with alignment constants
-    /**
-     *  This is simply a DetElement crawler...
-     *
-     *  \author  M.Frank
-     *  \version 1.0
-     *  \date    01/04/2016
-     */
-    struct AlignmentCreator : public DetectorProcessor {
-      ConditionsManager manager;
-      ConditionsPool*   pool;
-      /// Print level
-      PrintLevel        printLevel;
-      /// Constructor
-      AlignmentCreator(ConditionsManager m,ConditionsPool* p)
-        : manager(m), pool(p), printLevel(DEBUG) {}
-      /// Callback to process a single detector element
-      virtual int operator()(DetElement de, int level);
-    };
+using Conditions::UserPool;
+using Conditions::Condition;
+using Conditions::ConditionKey;
+using Conditions::ConditionsPool;
+using Conditions::ConditionsSlice;
+using Conditions::ConditionsManager;
+using Conditions::DetConditions;
 
-    // This is important, otherwise the register and forward calls won't find them!
-    /**
-     *  \author  M.Frank
-     *  \version 1.0
-     *  \date    01/04/2016
-     */
-    struct AlignmentKeys : public DetectorProcessor {
-      /// Constructor
-      AlignmentKeys() = default;
-      /// Callback to process a single detector element
-      virtual int operator()(DetElement de, int level);
-    };
+using Alignments::Delta;
+using Alignments::DetAlign;
+using Alignments::Alignment;
+using Alignments::AlignmentsManager;
 
-    /// Example how to access the alignment constants from a detector element
-    /**
-     *  \author  M.Frank
-     *  \version 1.0
-     *  \date    01/04/2016
-     */
-    struct AlignmentDataAccess : public Alignments::AlignmentsProcessor  {
-      UserPool& pool;
-      /// Print level
-      PrintLevel        printLevel;
-      /// Constructor
-      AlignmentDataAccess(UserPool& p) : AlignmentsProcessor(0), pool(p),
-                                         printLevel(DEBUG) {
-      }
-      /// Callback to process a single detector element
-      int processElement(DetElement de);
-    };
+/// Example how to populate the detector description with alignment constants
+/**
+ *  This is simply a DetElement crawler...
+ *
+ *  \author  M.Frank
+ *  \version 1.0
+ *  \date    01/04/2016
+ */
+struct AlignmentCreator : public DetectorProcessor {
+  ConditionsManager manager;
+  ConditionsPool *pool;
+  /// Print level
+  PrintLevel printLevel;
+  /// Constructor
+  AlignmentCreator(ConditionsManager m, ConditionsPool *p)
+      : manager(m), pool(p), printLevel(DEBUG) {}
+  /// Callback to process a single detector element
+  virtual int operator()(DetElement de, int level);
+};
 
-    /// Helper to run DetElement scans
-    /**
-     *  \author  M.Frank
-     *  \version 1.0
-     *  \date    01/04/2016
-     */
-    struct Scanner : public DetectorProcessor  {
-      DetElement::Processor* proc;
-      /// Callback to process a single detector element
-      virtual int operator()(DetElement de, int)
-      { return proc->processElement(de);                     }
-      template <typename Q> Scanner& scan(Q& p, DetElement start)
-      { Scanner obj; obj.proc = &p; obj.process(start, 0, true); return *this; }
-      template <typename Q> Scanner& scan2(const Q& p, DetElement start)
-      { Scanner obj; obj.proc = const_cast<Q*>(&p); obj.process(start, 0, true); return *this; }
-    };
+// This is important, otherwise the register and forward calls won't find them!
+/**
+ *  \author  M.Frank
+ *  \version 1.0
+ *  \date    01/04/2016
+ */
+struct AlignmentKeys : public DetectorProcessor {
+  /// Constructor
+  AlignmentKeys() = default;
+  /// Callback to process a single detector element
+  virtual int operator()(DetElement de, int level);
+};
 
-    /// Install the consitions and the alignment manager
-    void installManagers(LCDD& lcdd);
-    /// Register the alignment callbacks
-    void registerAlignmentCallbacks(LCDD& lcdd,
-                                    ConditionsSlice& slice,
-                                    Alignments::AlignmentsManager alignMgr);
+/// Example how to access the alignment constants from a detector element
+/**
+ *  \author  M.Frank
+ *  \version 1.0
+ *  \date    01/04/2016
+ */
+struct AlignmentDataAccess : public Alignments::AlignmentsProcessor {
+  UserPool &pool;
+  /// Print level
+  PrintLevel printLevel;
+  /// Constructor
+  AlignmentDataAccess(UserPool &p)
+      : AlignmentsProcessor(0), pool(p), printLevel(DEBUG) {}
+  /// Callback to process a single detector element
+  int processElement(DetElement de);
+};
 
-    
-  }       /* End namespace AlignmentExamples           */
-}         /* End namespace DD4hep                      */
-#endif    /* DD4HEP_ALIGNDET_ALIGNMENTEXAMPLEOBJECTS_H */
+/// Helper to run DetElement scans
+/**
+ *  \author  M.Frank
+ *  \version 1.0
+ *  \date    01/04/2016
+ */
+struct Scanner : public DetectorProcessor {
+  DetElement::Processor *proc;
+  /// Callback to process a single detector element
+  virtual int operator()(DetElement de, int) {
+    return proc->processElement(de);
+  }
+  template <typename Q> Scanner &scan(Q &p, DetElement start) {
+    Scanner obj;
+    obj.proc = &p;
+    obj.process(start, 0, true);
+    return *this;
+  }
+  template <typename Q> Scanner &scan2(const Q &p, DetElement start) {
+    Scanner obj;
+    obj.proc = const_cast<Q *>(&p);
+    obj.process(start, 0, true);
+    return *this;
+  }
+};
+
+/// Install the consitions and the alignment manager
+void installManagers(LCDD &lcdd);
+/// Register the alignment callbacks
+void registerAlignmentCallbacks(LCDD &lcdd, ConditionsSlice &slice,
+                                Alignments::AlignmentsManager alignMgr);
+
+} /* End namespace AlignmentExamples           */
+} /* End namespace DD4hep                      */
+#endif /* DD4HEP_ALIGNDET_ALIGNMENTEXAMPLEOBJECTS_H */
